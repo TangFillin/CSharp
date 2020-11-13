56f69180c5ddab1ab26ca7358e7926d952053826
Fillin <35187326+TangFillin@users.noreply.github.com>
2020-11-13 23:38:45 +0800

    客户端向服务器发送数据

--- CSharp/Multi_Client/Form_Client.Designer.cs
+++ CSharp/Multi_Client/Form_Client.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace Multi_Server
+namespace Multi_Client
 {
-    partial class Form_Server
+    partial class Form_Client
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,45 +29,45 @@ namespace Multi_Server
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_listen = new System.Windows.Forms.Button();
+            this.btn_connect = new System.Windows.Forms.Button();
             this.txt_IPAddress = new System.Windows.Forms.TextBox();
             this.txt_port = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txt_message = new System.Windows.Forms.TextBox();
-            this.txt_sendMessage = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.btn_sendMessage = new System.Windows.Forms.Button();
+            this.txt_send = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_listen
+            // btn_connect
             // 
-            this.btn_listen.Location = new System.Drawing.Point(277, 32);
-            this.btn_listen.Name = "btn_listen";
-            this.btn_listen.Size = new System.Drawing.Size(75, 23);
-            this.btn_listen.TabIndex = 0;
-            this.btn_listen.Text = "开始监听";
-            this.btn_listen.UseVisualStyleBackColor = true;
-            this.btn_listen.Click += new System.EventHandler(this.btn_listen_Click);
+            this.btn_connect.Location = new System.Drawing.Point(306, 30);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "连接";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
             // 
             // txt_IPAddress
             // 
-            this.txt_IPAddress.Location = new System.Drawing.Point(63, 29);
+            this.txt_IPAddress.Location = new System.Drawing.Point(78, 30);
             this.txt_IPAddress.Name = "txt_IPAddress";
             this.txt_IPAddress.Size = new System.Drawing.Size(100, 23);
             this.txt_IPAddress.TabIndex = 1;
             // 
             // txt_port
             // 
-            this.txt_port.Location = new System.Drawing.Point(219, 30);
+            this.txt_port.Location = new System.Drawing.Point(248, 29);
             this.txt_port.Name = "txt_port";
-            this.txt_port.Size = new System.Drawing.Size(53, 23);
+            this.txt_port.Size = new System.Drawing.Size(52, 23);
             this.txt_port.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 32);
+            this.label1.Location = new System.Drawing.Point(29, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 17);
             this.label1.TabIndex = 3;
@@ -76,7 +76,7 @@ namespace Multi_Server
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(170, 33);
+            this.label2.Location = new System.Drawing.Point(184, 33);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 17);
             this.label2.TabIndex = 4;
@@ -84,57 +84,57 @@ namespace Multi_Server
             // 
             // txt_message
             // 
-            this.txt_message.Location = new System.Drawing.Point(15, 99);
+            this.txt_message.Location = new System.Drawing.Point(29, 95);
             this.txt_message.Multiline = true;
             this.txt_message.Name = "txt_message";
             this.txt_message.ReadOnly = true;
             this.txt_message.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txt_message.Size = new System.Drawing.Size(337, 164);
+            this.txt_message.Size = new System.Drawing.Size(352, 199);
             this.txt_message.TabIndex = 5;
-            // 
-            // txt_sendMessage
-            // 
-            this.txt_sendMessage.Location = new System.Drawing.Point(15, 295);
-            this.txt_sendMessage.Multiline = true;
-            this.txt_sendMessage.Name = "txt_sendMessage";
-            this.txt_sendMessage.Size = new System.Drawing.Size(339, 89);
-            this.txt_sendMessage.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 68);
+            this.label3.Location = new System.Drawing.Point(29, 66);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 17);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "消息窗口";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "消息";
             // 
-            // btn_sendMessage
+            // txt_send
             // 
-            this.btn_sendMessage.Location = new System.Drawing.Point(277, 390);
-            this.btn_sendMessage.Name = "btn_sendMessage";
-            this.btn_sendMessage.Size = new System.Drawing.Size(75, 23);
-            this.btn_sendMessage.TabIndex = 9;
-            this.btn_sendMessage.Text = "发送消息";
-            this.btn_sendMessage.UseVisualStyleBackColor = true;
+            this.txt_send.Location = new System.Drawing.Point(29, 317);
+            this.txt_send.Multiline = true;
+            this.txt_send.Name = "txt_send";
+            this.txt_send.Size = new System.Drawing.Size(352, 58);
+            this.txt_send.TabIndex = 7;
             // 
-            // Form_Server
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(306, 381);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 8;
+            this.btn_send.Text = "发送";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form_Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_sendMessage);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_send);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txt_sendMessage);
             this.Controls.Add(this.txt_message);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_port);
             this.Controls.Add(this.txt_IPAddress);
-            this.Controls.Add(this.btn_listen);
-            this.Name = "Form_Server";
-            this.Text = "服务端";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form_Client";
+            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,19 +142,16 @@ namespace Multi_Server
 
         #endregion
 
-        private System.Windows.Forms.Button btn_listen;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.TextBox txt_IPAddress;
+        private System.Windows.Forms.TextBox txt_port;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txt_message;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button btn_sendMessage;
-        private System.Windows.Forms.TextBox txt_IPAddress;
-        private System.Windows.Forms.TextBox txt_port;
-        private System.Windows.Forms.TextBox txt_sendMessage;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txt_send;
     }
 }
 
---
+++ CSharp/Multi_Client/Form_Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_Client
+{
+    public partial class Form_Client : Form
+    {
+        public Form_Client()
+        {
+            InitializeComponent();
+        }
+        Socket socket;
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            string ip = txt_IPAddress.Text.Trim();
+            string port = txt_port.Text.Trim();
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddress = IPAddress.Parse(ip);
+            IPEndPoint point = new IPEndPoint(ipaddress, Convert.ToInt32(port));
+            socket.Connect(point);
+            ShowMessage("连接成功");
+        }
+
+        void ShowMessage(string message)
+        {
+            txt_message.Text += message + "\r\n";
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            string message = txt_send.Text.Trim();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            socket.Send(buffer);
+            ShowMessage("我：" + message);
+            txt_send.Clear();
+        }
+    }
+}
