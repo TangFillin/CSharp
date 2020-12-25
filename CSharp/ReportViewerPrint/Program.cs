f7f9f81b3642dd9904344e13c8a8e066cc1dd4b3
tangfeilang@cssj86.com <tangfeilang@cssj86.com>
2020-12-25 09:35:11 +0800

    防止重复启动

--- CSharp/ReportViewerPrint/Program.cs
+++ CSharp/ReportViewerPrint/Program.cs
@@ -14,6 +14,19 @@ namespace ReportViewerPrint
         [STAThread]
         static void Main(string[] args)
         {
+            //获取欲启动进程名
+            string strProcessName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            ////获取版本号 
+            //CommonData.VersionNumber = Application.ProductVersion; 
+            //检查进程是否已经启动，已经启动则显示报错信息退出程序。 
+            if (System.Diagnostics.Process.GetProcessesByName(strProcessName).Length > 1)
+            {
+                //SendCopyDataMessage(p.MainWindowHandle, Args[0]); //发送消息                  
+                
+                //MessageBox.Show("打印客户端已启动！", "消息", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                //Application.Exit();
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Print(args.ToList()));
